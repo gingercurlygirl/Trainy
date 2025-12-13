7404a3bf1c7799feb03fc256a65a1880e7dccaae
Ivana Lovric <ivana.lovric1314@gmail.com>
2025-12-13 15:38:53 +0100

    used UTF-8 for request

--- src/main/java/com/example/trainy/schedulers/TrafikverketTask.java
+++ src/main/java/com/example/trainy/schedulers/TrafikverketTask.java
@@ -8,6 +8,11 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+import java.nio.charset.StandardCharsets;
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
 @Component
 public class TrafikverketTask {
 
@@ -25,17 +30,40 @@ public class TrafikverketTask {
         String url = "https://api.trafikinfo.trafikverket.se/v2/data.json";
 
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.TEXT_HTML);
+        headers.setContentType(new MediaType("text", "xml", StandardCharsets.UTF_8));
 
         String api_key = System.getenv().getOrDefault("TRAFIKVERKET_API_KEY", "demokey");
+
+        OffsetDateTime now = OffsetDateTime.now(ZoneOffset.UTC);
+        OffsetDateTime end = now.plusDays(2).withHour(12).withMinute(0).withSecond(0);
+
+        DateTimeFormatter fmt = DateTimeFormatter.ISO_OFFSET_DATE_TIME;
+
+        String from = now.format(fmt);
+        String to = end.format(fmt);
 
         String htmlBody = String.format("""
                 <REQUEST>
                   <LOGIN authenticationkey="%s"/>
-                  <QUERY objecttype="TrainStationMessage" namespace="rail.trafficinfo" schemaversion="1" limit="10">
-                    <FILTER></FILTER>
+                  <QUERY objecttype="TrainAnnouncement" schemaversion="1.9" limit="10">
+                    <FILTER>
+                      <AND>
+                        <EQ name="InformationOwner" value="Mälardalstrafik AB" />
+                        <EQ name="LocationSignature" value="Vhd" />
+                        <OR>
+                          <AND>
+                            <LT name="AdvertisedTimeAtLocation" value="%s" />
+                            <GT name="AdvertisedTimeAtLocation" value="%s" />
+                          </AND>
+                          <AND>
+                            <LT name="EstimatedTimeAtLocation" value="%s" />
+                            <GT name="EstimatedTimeAtLocation" value="%s" />
+                          </AND>
+                        </OR>
+                      </AND>
+                    </FILTER>
                   </QUERY>
-                </REQUEST>""", api_key);
+                </REQUEST>""", api_key, to, from, to, from);
 
         HttpEntity<String> request = new HttpEntity<>(htmlBody, headers);
 
